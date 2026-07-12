6a8debfcd56f9868bf9f8c0c07f4fa0aa3718705
ghaith98 <gaitho_23@hotmail.com>
2026-07-12 22:36:59 +0300

    Update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
-
 import "./globals.css";
 import LayoutShell from "./LayoutShell";
 import { LanguageProvider } from "../context/LanguageContext";
-import TawkUserSync from "./TawkUserSync";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,31 +33,6 @@ export default function RootLayout({
         <LanguageProvider>
           <LayoutShell>{children}</LayoutShell>
         </LanguageProvider>
-
-        <TawkUserSync />
-
-        <Script
-          id="tawk-to-chat"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var Tawk_API = Tawk_API || {};
-              var Tawk_LoadStart = new Date();
-
-              (function () {
-                var s1 = document.createElement("script");
-                var s0 = document.getElementsByTagName("script")[0];
-
-                s1.async = true;
-                s1.src = "https://embed.tawk.to/6a527c249a72601d480b42c2/1jt939t48";
-                s1.charset = "UTF-8";
-                s1.setAttribute("crossorigin", "*");
-
-                s0.parentNode.insertBefore(s1, s0);
-              })();
-            `,
-          }}
-        />
       </body>
     </html>
   );
